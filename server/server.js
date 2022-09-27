b44ccbfa94170428d24340c266fad5f0fdfc941c
Juan Villegas <jvillegas422@gmail.com>
2022-09-27 16:19:03 -0500

    Update server

--- server/server.js
+++ server/server.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const app = express();
 
+// Make files in the server/public folder
+// available to who wants some
+app.use(express.static('server/public'));
+
 // app.listen(3000, () => {
 //     console.log('We\'re live!');
 // });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log('I\'m listening!');
-});
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('listening on port', port);
+  });
 
 console.log('In server.js!');
